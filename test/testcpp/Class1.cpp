40bab6dd9608da31c575841d2294f5806a49cb18
Francesco Pretto <francesco.pretto@it-robotics.it>
2015-03-21 18:59:30 +0100

    Fixed some mistakes in the Class1.cpp test

--- test/testcpp/Class1.cpp
+++ test/testcpp/Class1.cpp
@@ -10,7 +10,7 @@ using namespace NS1;
 ITR_DEFINE_CLASS_LOGGER(Class1);
 
 // Static loggers are needed for free methods
-ITR_DEFINE_STATIC_LOGGER(Class1Static);
+ITR_DEFINE_STATIC_LOGGER(NS1.Class1Static);
 
 static void foo_free();
 
@@ -18,7 +18,7 @@ void Class1::foo1()
 {
   ITR_LOG_FLOW_BEGIN();
 
-  cout << "Enabled MAXDETAIL Class2 = " << (ITR_ENABLED_MAXDETAIL() ? "True" : "False") << endl;
+  cout << "Enabled MAXDETAIL Class1 = " << (ITR_ENABLED_MAXDETAIL() ? "True" : "False") << endl;
   cout << "Enabled DETAIL Class1 = " << (ITR_ENABLED_DETAIL() ? "True" : "False") << endl;
 
   // Will use Class1 logger
@@ -30,8 +30,8 @@ void Class1::foo1()
   // Will use Class1Cust logger
   ITR_LOG_MAXDETAIL("Class1Cust", "Test log");
   ITR_LOG_MAXDETAIL_FORCED("Test log");
-  ITR_LOG_DETAIL("Class1Cust", "Test log");
-  ITR_LOG_DETAIL("Class1Cust", "Test log with integer : " << 1);
+  ITR_LOG_DETAIL("NS1.Class1Cust", "Test log");
+  ITR_LOG_DETAIL("NS1.Class1Cust", "Test log with integer : " << 1);
 
   foo_static();
 
@@ -41,7 +41,7 @@ void Class1::foo1()
 void Class1::foo2()
 {
   // Function loggers overrides any other static or class logger
-  ITR_DEFINE_FUNCTION_LOGGER(Class1Func);
+  ITR_DEFINE_FUNCTION_LOGGER(NS1.Class1Func);
 
   // Will use Class1Func logger
   ITR_LOG_MAXDETAIL("Test log");
